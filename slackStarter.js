ea61837ceec1b6cd8d973aa8dfafa0a29b020bbd
Tyrone Tan <new_overlord@yahoo.com>
2016-10-03 00:13:06 +0800

    no message

--- slackStarter.js
+++ slackStarter.js
@@ -1,36 +1,22 @@
+var Slack = require('slack');
 var Settings = require('./settings.json');
 var ToggleTimeCheck = require('TogglTimeCheck');
 var team = Settings.teamname;
 var botToken = Settings.botToken;
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 
-var getTeamList = function() {
-  var url = "https://" + team + ".slack.com/api/users.list?token=" + botToken;
-  var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var jsonResponse = JSON.parse(xhr.responseText);
-      var members = jsonResponse.members;
-      for (var i = 0; i < members.length; i++) {
-        var member = members[i];
-        var now = new Date();
-        ToggleTimeCheck.getTimeSpent(now.getDate() - 7, now, member.profile.email, function(err, time) {
-          if (time < Settings.minHours) {
-            var dm = new XMLHttpRequest();
-
-            var dmUrl = "https://" + team + ".slack.com/api/chat.postMessage?token=" + botToken + "&channel=" + settings.channelID + "&text=";
-            dm.onreadystatechange = function() {
-              if (dm.readyState == 4 && dm.status == 200) {
-              }
-            };
-            dm.open("GET", dmUrl, true;)
-          }
-        });
-      }
+var weeklyToggl = function() {
+  slack.users.list({token: botToken}, function(err, data) {
+    var members = data.members;
+    for (var i = 0; i < members.length; i++) {
+      var member = members[i];
+      var now = new Date();
+      ToggleTimeCheck.getTimeSpent(now.getDate() - 7, now, member.profile.email, function(err, time) {
+        if (time < Settings.minHours) {
+          var text = "You have recorded " + time + " work hours for the week, and are behind the minimum hours by " + Settings.minHours - time + " hours";
+          slack.chat.postMessage({token: botToken, channel: Settings.channelID, text: text});
+        }
+      });
     }
-    xhr.open("GET", url, true);
-    xhr.send();
-  };
+  });
 };
