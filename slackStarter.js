62ed5ea139d0b1a3db0d4b8f887df96cd29a1be5
Michael King <mikerobertking@gmail.com>
2016-10-03 01:12:57 +0800

    no message

--- slackStarter.js
+++ slackStarter.js
@@ -1,22 +1,27 @@
-var Slack = require('slack');
+var slack = require('slack');
 var Settings = require('./settings.json');
-var ToggleTimeCheck = require('TogglTimeCheck');
+var ToggleTimeCheck = require('./togglTestFile.js');
+console.log(ToggleTimeCheck.getTimeSpent);
 var team = Settings.teamname;
-var botToken = Settings.botToken;
+var botToken = 'xoxp-26250155218-26230467748-86427991783-e79f6b5172f1a30998ad850b67ef81d4';
 
-
-var weeklyToggl = function() {
+//var weeklyToggl = function() {
   slack.users.list({token: botToken}, function(err, data) {
+    if(err)
+      throw err;
+
     var members = data.members;
+  //  console.log(data.members);
     for (var i = 0; i < members.length; i++) {
       var member = members[i];
       var now = new Date();
-      ToggleTimeCheck.getTimeSpent(now.getDate() - 7, now, member.profile.email, function(err, time) {
+      ToggleTimeCheck.getTimeSpent(now, now, member.profile.email, function(err, time) {
         if (time < Settings.minHours) {
-          var text = "You have recorded " + time + " work hours for the week, and are behind the minimum hours by " + Settings.minHours - time + " hours";
+          var text = "You have recorded " + time + " work hours for the week, and are behind the minimum hours by " + (Settings.minHours - time) + " hours";
+          console.log(text);
           slack.chat.postMessage({token: botToken, channel: Settings.channelID, text: text});
         }
       });
     }
   });
-};
+//};
--- togglTestFile.js
+++ togglTestFile.js
@@ -3,52 +3,57 @@ var Settings = require('./settings.json');
 
 var toggl = new TogglClient({apiToken: Settings.toggleAPItoken});
 
+module.exports = {
+  // very inefficient dirty function for getting hours
+  // returns hours
+  getTimeSpent: function (start, end, email, callback) {
+    toggl.getWorkspaces(function(err, workspaces) {
 
-// very inefficient dirty function for getting hours
-// returns hours
-function getTimeSpent(start, end, email, callback) {
-  toggl.getWorkspaces(function(err, workspaces) {
-
-    // dirty throw for now
-    if(err) {
-      throw err;
-    }
-
-    // get workspace names
-    for (var i = 0; i < workspaces.length; i++) {
-      //console.log(workspaces);
-      if(Settings.toggleWorkspaceName.indexOf(workspaces[i].name) > -1) {
-          console.log('found the workspace');
-          // get workspace users
-          toggl.getWorkspaceUsers(workspaces[i].id, function(err, users) {
-            //console.log(users);
-            var foundUser = false;
-            for (var i = 0; i < users.length; i++) {
-              if(users[i].email == email) {
-                console.log('found user with email ' + email);
-                toggl.summaryReport({"grouping": "users", "workspace_id": users[i].default_wid, "user_ids": users[i].id, "since": start, "until": end}, function(err, report) {
-                  callback(undefined, report.total_grand / 3600000);
-                });
-                foundUser = true;
-                break;
-              }
-            }
-            if(!foundUser) {
-              callback("Unable to find user");
-            }
-          });
+      // dirty throw for now
+      if(err) {
+        throw err;
       }
-    }
-  });
-}
+
+      // get workspace names
+      for (var i = 0; i < workspaces.length; i++) {
+        //console.log(workspaces);
+        if(Settings.toggleWorkspaceName.indexOf(workspaces[i].name) > -1) {
+            console.log('found the workspace');
+            // get workspace users
+            toggl.getWorkspaceUsers(workspaces[i].id, function(err, users) {
+              //console.log(users);
+              var foundUser = false;
+              for (var i = 0; i < users.length; i++) {
+                if(users[i].email == email) {
+                  console.log(start.toISOString().slice(0,10));
+                  console.log(end.toISOString().slice(0,10));
+
+                  toggl.summaryReport({"grouping": "users", "workspace_id": users[i].default_wid, "user_ids": users[i].id, "since": start.toISOString().slice(0,10), "until": end.toISOString().slice(0,10)}, function(err, report) {
+                    if(err) {
+                      console.log('stupid error');
+                      console.log(err);
+                    }
+                    callback(undefined, report.total_grand / 3600000);
+                  });
+                  foundUser = true;
+                  break;
+                }
+              }
+              if(!foundUser) {
+                callback("Unable to find user");
+              }
+            });
+        }
+      }
+    });
+  }
+};
 
 // example calls
-getTimeSpent('2016-07-01', '2016-12-01', 'mikerobertking@gmail.com', function(err, time) {
+/*getTimeSpent('2016-07-01', '2016-12-01', 'mikerobertking@gmail.com', function(err, time) {
   console.log("got Mike's hours: " + time);
 });
 getTimeSpent('2016-07-01', '2016-12-01', 'new.overlord@gmail.com', function(err, time) {
   console.log("got Tyrone's hours: " + time);
 });
-
-
-module.exports = TogglTimeCheck;
+*/
