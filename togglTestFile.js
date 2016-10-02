f2a314caeb064414d063fb6accd59fca986c545a
Michael King <mikerobertking@gmail.com>
2016-10-02 19:50:02 +0800

    no message

--- togglTestFile.js
+++ togglTestFile.js
@@ -5,13 +5,18 @@ var toggl = new TogglClient({apiToken: Settings.toggleAPItoken});
 
 toggl.getWorkspaces(function(err, workspaces) {
 
+  // dirty throw for now
+  if(err) {
+    throw err;
+  }
+
   // get workspace names
   for (var i = 0; i < workspaces.length; i++) {
     console.log(workspaces);
     if(Settings.toggleWorkspaceName.indexOf(workspaces[i].name) > -1) {
         console.log('found the workspace');
         // get workspace users
-        toggl.getWorkspaceUsers(workspace[i].id, function(err, users) {
+        toggl.getWorkspaceUsers(workspaces[i].id, function(err, users) {
           console.log('found some users');
           console.log(users);
 
