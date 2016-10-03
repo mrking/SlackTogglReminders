6f72831a4f562928276f278b2937186b6c43afcb
Tyrone Tan <new_overlord@yahoo.com>
2016-10-03 19:31:45 +0800

    no message

--- slackStarter.js
+++ slackStarter.js
@@ -4,9 +4,9 @@ var Settings = require('./settings.json');
 var ToggleTimeCheck = require('./togglTestFile.js');
 console.log(ToggleTimeCheck.getTimeSpent);
 var team = Settings.teamname;
-var botToken = 'xoxb-86540983813-wvuB3y64w2jR0xGwK5DEfqmE';
+var botToken = 'xoxb-86540983813-bMH5i4aNvWV0LhIiv1J9qd8J';
 
-
+// setInterval(function() {
   slack.users.list({token: botToken}, function(err, data) {
     if(err) {
       console.log(err);
@@ -26,11 +26,16 @@ var botToken = 'xoxb-86540983813-wvuB3y64w2jR0xGwK5DEfqmE';
       console.log(member);
       ToggleTimeCheck.getTimeSpent(weekBefore, now, member.profile.email, function(err, time) {
         if (time < Settings.minHours) {
-          var text = "You have recorded " + time + " work hours for the week, and are behind the minimum hours by " + (Settings.minHours - time) + " hours";
+          var text = member.profile.real_name + " has recorded " + time + " work hours for the week, and are behind the minimum hours by " + (Settings.minHours - time) + " hours";
           slack.chat.postMessage({token: botToken, channel: Settings.channelID, text: text});
+          slack.chat.postMessage({token: botToken, channel: member.id, text: text});
           console.log(text);
+        } else {
+            var text = member.profile.real_name + " has recorded " + time + " work hours for the week";
+            slack.chat.postMessage({token: botToken, channel: Settings.channelID, text: text});
+            console.log(text);
         }
       });
     }
   });
-//};
+// }, 3600000);
