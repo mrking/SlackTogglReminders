00c2105682cd38a41280ab809fbfb2c442eeed00
Tyrone Tan <new_overlord@yahoo.com>
2016-10-04 20:57:55 +0800

    added toggl scheduler at bot init

--- slackStarter.js
+++ slackStarter.js
@@ -1,8 +1,11 @@
 var slack = require('slack');
+var moment = require('moment');
+var ToggleTimeCheck = require('./togglTestFile.js');
 //var dotenv = require('dotenv');
 //dotenv.load();
 var bot = slack.rtm.client();
 var token = process.env.SLACK_TOKEN;
+
 
 // logs: ws, started, close, listen, etc... in addition to the RTM event handler methods
 console.log(Object.keys(bot));
@@ -10,6 +13,41 @@ console.log(Object.keys(bot));
 // do something with the rtm.start payload
 bot.started(function(payload) {
   console.log('payload from rtm.start', payload);
+
+  //schedule the toggl notifications at bot init?
+  setInterval(function() {
+    if (moment().diff(moment().endOf('week')) <= 3600000) { //if time now is less than one hour to the end of the week, run code
+      console.log(moment());
+
+      slack.users.list({token: token}, function(err, data) {
+        if(err) {
+          console.log(err);
+          throw err;
+        }
+
+        var members = data.members;
+        var now = new moment();
+        var weekBefore = new moment().subtract(7, "days");
+
+        // console.log(data.members);
+        for (var i = 0; i < members.length; i++) {
+          var member = members[i];
+          if (member.is_bot)
+            continue;
+
+          //console.log(member);
+          ToggleTimeCheck.getTimeSpent(weekBefore, now, member.profile.email, function(err, time) {
+            if (time < Settings.minHours) {
+              var text = "You have recorded " + time + " work hours for the week, and are behind the minimum hours by " + (Settings.minHours - time) + " hours";
+              //console.log('before post a message');
+              slack.chat.postMessage({token: token, channel: "bizdev", text: text});
+              //console.log(text);
+            }
+          });
+        }
+      });
+    }
+  }, 3600000);
 });
 
 // respond to a user_typing message
@@ -22,7 +60,6 @@ bot.user_typing(function(msg) {
 bot.listen({token:token});
 /*
 var Settings = require('./settings.json');
-var moment = require('moment');
 var ToggleTimeCheck = require('./togglTestFile.js');
 var team = Settings.teamname;
 
