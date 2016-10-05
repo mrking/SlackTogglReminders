90096cd8f9a8e844e4384b9b2a387d2e92cb4afc
Tyrone Tan <new_overlord@yahoo.com>
2016-10-05 21:45:24 +0800

    no message

--- slackStarter.js
+++ slackStarter.js
@@ -24,26 +24,26 @@ bot.message(function(message) {
   //  return;
   //}
 
-  if (ValidateMessage(message.text)) {
-    var commands = message.text.split(' ');
-
-    switch(commands[0]) {
-      case 'startNotifications':    //startNotifications [minHours] [checkFrequency]
-        if (commands[1] && parseInt(commands[1])) {
-          USER_MIN_HOURS = parseInt(commands[1]);
-        }   //since the [minHours] parameter is an optional field, allow them to ignore the parameter or write the wrogn flags
-        if (commands[2] && parseInt(commands[2])) {
-            USER_MIN_HOURS_CHECK_FREQUENCY = parseInt(commands[2]);
-        } //since the [minHours] parameter is an optional field, allow them to ignore the parameter or write the wrogn flags
-        setInterval(RunUserHoursCheck, USER_MIN_HOURS_CHECK_FREQUENCY);
-        break;
-      case 'help':
-        slackAPI.postMessageToChannel('f u I don\'t help');
-        break;
-      default:
-        break;
-    }
-  }
+  // if (ValidateMessage(message.text)) {
+  //   var commands = message.text.split(' ');
+  //
+  //   switch(commands[0]) {
+  //     case 'startNotifications':    //startNotifications [minHours] [checkFrequency]
+  //       if (commands[1] && parseInt(commands[1])) {
+  //         USER_MIN_HOURS = parseInt(commands[1]);
+  //       }   //since the [minHours] parameter is an optional field, allow them to ignore the parameter or write the wrogn flags
+  //       if (commands[2] && parseInt(commands[2])) {
+  //           USER_MIN_HOURS_CHECK_FREQUENCY = parseInt(commands[2]);
+  //       } //since the [minHours] parameter is an optional field, allow them to ignore the parameter or write the wrogn flags
+  //       setInterval(RunUserHoursCheck, USER_MIN_HOURS_CHECK_FREQUENCY);
+  //       break;
+  //     case 'help':
+  //       slackAPI.postMessageToChannel('f u I don\'t help');
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 });
 
 
@@ -73,7 +73,7 @@ function RunUserHoursCheck() {
             toggl.getTimeSpent(weekBefore, now, member.profile.email).then(function(time) {
 
                 if (time < USER_MIN_HOURS) {
-                  
+
                     var text = member.real_name + " has recorded " + time.toPrecision(3) + " work hours for the week, and are behind the minimum hours by " + (USER_MIN_HOURS - time).toPrecision(3) + " hours";
                     //slackAPI.sendNotification(SLACK_CHANNEL_NAME, 'MINIMUM_TIME_DEFICIT', text);
                     slackAPI.sendNotification(member.id, 'MINIMUM_TIME_DEFICIT', text, true);
