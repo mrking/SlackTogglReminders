15229d5b630d3e6905dcb89d713e211ceb5fdedc
Michael King <mikerobertking@gmail.com>
2016-10-08 19:54:55 +0800

    no message

--- app/Toggl.js
+++ app/Toggl.js
@@ -21,22 +21,32 @@ var self = module.exports = {
             // test ws ID 1382104
             self.getWorkspaceID(TOGGL_WORKSPACE_NAME).then(function(wsID) {
               toggl.getWorkspaceUsers(wsID, function(err, users) {
+
                 if(err) {
+                  console.error('ran into a problem getting to workspace users ' + err);
                   reject (err);
+                  return;
                 }
 
+                // Save users into our cache object
                 for (var i = 0; i <  users.length; i++) {
                   UsersInToggl[users[i].email] = users[i];
                 }
 
                 if(UsersInToggl[email]) {
+                  console.info('found our look up user using email ' + email);
                   resolve(UsersInToggl[email]);
+                  return;
                 } else {
+
+                  console.info('did not find our look up user using email ' + email);
                   reject('unable to find ' + email +  ' in toggl');
+                  return;
                 }
               });
-            }, function() {
+            }, function(err) {
               console.error ('Unable to find workspace ID for: ' + TOGGL_WORKSPACE_NAME);
+              reject(err);
             });
         });
       }
@@ -49,16 +59,16 @@ var self = module.exports = {
         return new Promise(function (resolve, reject) {
           toggl.getWorkspaces(function(err, workspaces) {
             for (var i = 0; i < workspaces.length; i++) {
-
               if(workspaceName == workspaces[i].name) {
                  WorkspaceID = workspaces[i].id;
+                 console.info('Was able to resolve console ID to be ' + WorkspaceID);
                  resolve(WorkspaceID);
+                 return;
                }
              }
 
-             if(WorkspaceID == -1 ) {
+               console.info('Was unable to find workspace with name: ' +  workspaceName);
                reject('Was unable to find workspace with name: ' +  workspaceName);
-             }
            });
         });
       }
@@ -81,14 +91,14 @@ var self = module.exports = {
 
 
 
-/*
- console.log('test getTimeSpent for Michael');
- module.exports.getTimeSpent(new Date(2016,01,01), new Date(), 'mikerobertking@gmail.com').then(function(response) {
+
+// console.log('test getTimeSpent for Michael');
+/* module.exports.getTimeSpent(new Date(2016,01,01), new Date(), 'mikerobertking@gmail.com').then(function(response) {
    console.log("Success! Hours spent: ", response);
  }, function(error) {
    console.error("Failed!", error);
- });
-*/
+ });*/
+
 
 // example calls
 /*getTimeSpent('2016-07-01', '2016-12-01', 'mikerobertking@gmail.com', function(err, time) {
--- test/toggl.js
+++ test/toggl.js
@@ -1,24 +1,28 @@
 var expect = require("chai").expect;
 var togglAPI = require("../app/Toggl.js");
 
-describe("Our toggl API test account ", function() {
-  this.timeout(30000);
+describe("Our toggl API test account", function() {
+  this.timeout(10000);
   describe("users", function() {
       it("shouldn't be cached yet on init", function() {
           expect(togglAPI.getCachedUsers()).to.be.empty;
       });
       it("should have mike and then one user cached", function(done) {
+          togglAPI.getUser('mikerobertking@gmail.com').then(function() {
+            console.log('this part seems to work');
+          });
           return togglAPI.getUser('mikerobertking@gmail.com').then(function(user) {
-
+            console('but we never reach here for some reason');
               expect(user).to.exist;
-              expect(user.email).to.be.equal('mikerobertking@gmail.com');
-              expect(togglAPI.getCachedUsers().length).to.be.equal(1);
+              expect(user.email).to.equal('mikerobertking@gmail.com');
+              expect(togglAPI.getCachedUsers()).to.exist;
+              expect(togglAPI.getCachedUsers().length).to.equal(1);
               done();
           }, function(err) {
             assert.isNotOk(err,'ran into error trying to get mike');
             done();
           });
-      });
+      });/*
       it("shouldn't have fakemike", function(done) {
           return togglAPI.getUser('fakemikerobertking@gmail.com').then(function(users) {
 
@@ -37,6 +41,6 @@ describe("Our toggl API test account ", function() {
               done();
               //expect(user.email).to.equals("new.overlord@gmail.com")
           });
-      });
+      });*/
   });
 });
